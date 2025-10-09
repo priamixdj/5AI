afe863f09c33488dd3a7b5679812ec34b34c0cac
matteopriarolo <matteo.priarolo@istitutoeinstein.eu>
2025-10-09 09:47:14 +0200

    nuovo

---
+++ TPSIT/Html/validazione/script.js
@@ -0,0 +1,31 @@
+function valida() {
+    validaNome();
+    validaCognome();
+    validaMail();
+    validaPassword();
+    validaConfermaPsw();
+    validaIndirizzo();
+    validaPagamento();
+}
+
+function validaNome(){
+    
+}
+function validaCognome(){
+    
+}
+function validaMail(){
+    
+}
+function validaPassword(){
+    
+}
+function validaConfermaPsw(){
+    
+}
+function validaIndirizzo(){
+    
+}
+function validaPagamento(){
+    
+}
